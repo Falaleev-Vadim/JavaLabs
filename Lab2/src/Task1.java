543ecbc838b869ea49ec03cc500a3f5f55f1f1e4
Vadim <falaleev.vadim.02@gmail.com>
2024-10-07 17:14:09 +0500

    Task2

--- Lab2/src/Task1.java
+++ Lab2/src/Task1.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        // Вариант 9
+        // 1/(n^2 - 1)
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число N, N >= 2: ");
+        String NString = scanner.nextLine();
 
+        int N;
+        try {
+            N = Integer.parseInt(NString);
+
+            if (N < 2){
+                throw new NumberFormatException("N должен быть больше 2");
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        double sum = 0;
+
+        for (int i = 2; i <= N; i++){
+            sum += 1 / (Math.pow(i, 2) - 1);
+        }
+
+        System.out.print(sum);
     }
 }
---
+++ Lab2/src/Task2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        //Вариант 9
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число X, |X| < 1: ");
+        String X = scanner.nextLine();
+        System.out.print("Введите целое число N, N > 0: ");
+        String N = scanner.nextLine();
+
+        try
+        {
+            if (Math.abs(Double.parseDouble(X)) >= 1) {
+                throw new NumberFormatException("Модуль Х должен быть меньше единицы");
+            }
+            if (Integer.parseInt(N) <= 0) {
+                throw new NumberFormatException("Число N должно быть больше 0");
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        double newX = Double.parseDouble(X);
+        int newN = Integer.parseInt(N);
+        double sum = newX;
+
+        if (newN == 1) {
+            System.out.println(sum);
+        }
+        else {
+            for (int i = 2; i <= newN; i++) {
+                sum += Math.pow(-1, i-1) * Math.pow(newX, i) / i;
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
